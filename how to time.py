a375ebdb892347be6929333de4a3aae8486e4848
Prateek-python <91688624+Prateek-python@users.noreply.github.com>
2022-10-10 19:41:06 +0530

    Add files via upload

---
+++ how to time.py
@@ -0,0 +1,9 @@
+import time
+local_time=time.ctime()
+print("local time",local_time)
+
+# by this method we can calculate or compare time passed in given process
+# intial=time.time()
+# print("time to run for loop ",time.time()-intial)
+# for i in range(0,5):
+#     print("..................")
